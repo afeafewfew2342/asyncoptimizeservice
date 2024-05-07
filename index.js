c449769d0cd414c3950896081c5f889657299b1d
afeafewfew2342 <js1lmfaowhy@hotmail.com>
2024-05-07 08:29:58 +0700

    update project

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
-  }
-  return nums[left];
+function add(x, y) {
+  return x + y;
 }
+
+const result = add(5, 3);
+console.log(result); // 8
